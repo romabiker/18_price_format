a69c3d6e8332994209f2bd24616de0c02b7f0e5a
Valery Ramusik <valer2677.@gmailcom>
2017-08-19 23:32:04 +0300

    Implement tests.py

--- tests.py
+++ tests.py
@@ -1,1 +1,22 @@
-# TODO place your tests here
+import unittest
+from format_price import format_price_for_site
+
+
+class TestFormatPrice(unittest.TestCase):
+
+    def test_format_price_for_site(self):
+        self.assertEqual('1 235', format_price_for_site('1234.9787'))
+        self.assertEqual('0', format_price_for_site('0'))
+        self.assertEqual('132 746 872', format_price_for_site('132746872'))
+        self.assertEqual('12 349 787', format_price_for_site('12349787.'))
+        self.assertEqual('8', format_price_for_site('7.5'))
+        self.assertEqual('7', format_price_for_site('7.4'))
+        self.assertIsNone(format_price_for_site('123.4978.7'))
+        self.assertIsNone(format_price_for_site('1234,9787'))
+        self.assertIsNone(format_price_for_site('-12349787'))
+        self.assertIsNone(format_price_for_site('asfsfsaf'))
+        self.assertIsNone(format_price_for_site('-0.1'))
+
+
+if __name__ == '__main__':
+    unittest.main()
